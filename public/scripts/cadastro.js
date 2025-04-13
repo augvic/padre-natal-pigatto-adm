1f3e9de044c77791dbd00bf23c479f32c8d557c9
Augusto Viceli <89478688+augvic@users.noreply.github.com>
2025-04-13 20:05:18 -0300

    Criado cadastro funcional no Firebase.

--- public/scripts/cadastro.js
+++ public/scripts/cadastro.js
@@ -1,28 +1,41 @@
-import { getFirestore, collection, addDoc, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js';
+// ================================================== //
+
+// ~~ Importações do Firebase.
+import { getFirestore, collection, setDoc, doc, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js';
 import { db } from '../firebase/firebase.js';
 
+// ~~ Coleta elementos.
 const form = document.getElementById('register-form');
+const nome = document.getElementById('nome');
+const cargo = document.getElementById('cargo');
 const enviadoAprovacao = document.getElementById('cadastro-aprovacao');
 const erroAprovacao = document.getElementById('erro-aprovacao');
 
+// ~~ Event listener no formulário para solicitar um cadastro.
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
-
+    const nome = form.nome.value;
+    const cargo = form.cargo.value;
     const email = form.email.value;
     const password = form.password.value;
-
     try {
-        await addDoc(collection(db, 'cadastros_pendentes'), {
-            email: email,
-            senha: password,
-            aprovado: false
-        });
+        const docId = crypto.randomUUID();
+        await setDoc(doc(db, 'cadastros_pendentes', docId),
+            {
+                nome: nome,
+                cargo: cargo,
+                email: email,
+                senha: password,
+                aprovado: false
+            }
+        );
         console.log('Cadastro enviado para aprovação.')
         enviadoAprovacao.style.display = 'block';
         form.reset();
-
     } catch (error) {
         console.error('Erro ao registrar cadastro pendente:', error);
         erroAprovacao.style.display = 'block';
     }
 });
+
+// ================================================== //
